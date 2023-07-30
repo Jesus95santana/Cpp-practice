ac32ce2bf4fb543470749a42d0bd21e14a5b5220
jesus95santana <jesus95santana@yahoo.com>
2023-07-30 11:21:08 -0400

    coin flipper

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
+#include <ctime>
 
-int main() {
-  
-  double tempf;
-  double tempc;
-  
-  // Ask the user
-  std::cout << "Enter the temperature in Fahrenheit:\n";
-  std::cin >> tempf;
-  
-  
-  tempc = (tempf - 32) / 1.8;
-  
-  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
-  
+int main()
+{
+
+    // Create a number that's 0 or 1
+
+    srand(time(NULL));
+    int coin = rand() % 2;
+
+    // If number is 0: Heads
+    // If it is not 0: Tails
+
+    if (coin == 0)
+    {
+
+        std::cout << "Heads\n";
+    }
+    else
+    {
+
+        std::cout << "Tails\n";
+    }
 }
